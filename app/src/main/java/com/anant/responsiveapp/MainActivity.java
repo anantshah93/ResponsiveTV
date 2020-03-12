f27b257015ad3b662d753003267da56d3f7fcb55
Anant Shah <anant.shah@ltts.com>
2020-03-12 18:43:21 +0530

    2nd screen and dimens rearrange code.

--- app/src/main/java/com/anant/responsiveapp/MainActivity.java
+++ app/src/main/java/com/anant/responsiveapp/MainActivity.java
@@ -3,11 +3,16 @@ package com.anant.responsiveapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = MainActivity.class.getSimpleName();
     AppCompatButton btnActivity1;
     AppCompatButton btnActivity2;
 
@@ -19,8 +24,58 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnActivity2 = findViewById(R.id.btnActivity2);
         btnActivity1.setOnClickListener(this);
         btnActivity2.setOnClickListener(this);
-    }
 
+        Log.e(TAG, "getDensity:"+getDensity(this));
+        Log.e(TAG, "getDensityName:"+getDensityName(this));
+    }
+    private static String getDensityName(Context context) {
+        float density = context.getResources().getDisplayMetrics().density;
+        if (density >= 4.0) {
+            return "xxxhdpi";
+        }
+        if (density >= 3.0) {
+            return "xxhdpi";
+        }
+        if (density >= 2.0) {
+            return "xhdpi";
+        }
+        if (density >= 1.5) {
+            return "hdpi";
+        }
+        if (density >= 1.0) {
+            return "mdpi";
+        }
+        return "ldpi";
+    }
+    public static String getDensity(Context context) {
+        String r;
+        DisplayMetrics metrics = new DisplayMetrics();
+
+        if (!(context instanceof Activity)) {
+            r = "no-dpi";
+        } else {
+            Activity activity = (Activity) context;
+            activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+            if (metrics.densityDpi <= DisplayMetrics.DENSITY_LOW) {
+                r = "ldpi";
+            } else if (metrics.densityDpi <= DisplayMetrics.DENSITY_MEDIUM) {
+                r = "mdpi";
+            }else if (metrics.densityDpi <= DisplayMetrics.DENSITY_HIGH) {
+                r = "hdpi";
+            }else if (metrics.densityDpi <= DisplayMetrics.DENSITY_XHIGH) {
+                r = "xhdpi";
+            } else if (metrics.densityDpi <= DisplayMetrics.DENSITY_XXHIGH) {
+                r = "xxhdpi";
+            }else if (metrics.densityDpi <= DisplayMetrics.DENSITY_XXXHIGH) {
+                r = "xxxhdpi";
+            } else {
+                r = "no-dpi";
+            }
+        }
+
+        return r;
+    }
     @Override
     public void onClick(View v) {
         Intent intent;
