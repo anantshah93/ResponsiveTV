edcca1068342867f5bad769665b943c53db8a877
anant-shah-ltts <anant.shah@ltts.com>
2020-04-16 01:33:11 +0530

    logic added to generate dimens file programmatically based on diff bucket

--- app/src/main/java/com/anant/responsiveapp/MainActivity.java
+++ app/src/main/java/com/anant/responsiveapp/MainActivity.java
@@ -1,8 +1,5 @@
 package com.anant.responsiveapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatButton;
-
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -10,6 +7,15 @@ import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -25,9 +31,60 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnActivity1.setOnClickListener(this);
         btnActivity2.setOnClickListener(this);
 
-        Log.e(TAG, "getDensity:"+getDensity(this));
-        Log.e(TAG, "getDensityName:"+getDensityName(this));
+        Log.e(TAG, "getDensity:" + getDensity(this));
+        Log.e(TAG, "getDensityName:" + getDensityName(this));
+
+        getDimens();
     }
+
+    private void getDimens() {
+        try {
+            float dpi = (float) 1;
+            //1 - mdpi, 0.67 - hdpi, 0.5 - xhdpi, 0.33 - xxhdpi, 0.25 - xxxhdpi
+            DecimalFormat formater = new DecimalFormat("#.0#");
+
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 1; i < 1000; i++) {
+                float finalVal = Float.parseFloat(formater.format(i * dpi));
+                String val = "<dimen name=\"_" + i + "dp\">" + finalVal + "dp</dimen>\n";
+                stringBuilder.append(val);
+            }
+
+            saveFile(stringBuilder);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*
+    * Give permission direct from settings
+    * */
+    private void saveFile(StringBuilder stringBuilder) {
+        //File logFile = new File("sdcard/Download/xxxhlog.txt");
+        //File logFile = new File("sdcard/Download/xxhlog.txt");
+        //File logFile = new File("sdcard/Download/xhlog.txt");
+        //File logFile = new File("sdcard/Download/hlog.txt");
+        File logFile = new File("sdcard/Download/mlog.txt");
+        if (!logFile.exists()) {
+            try {
+                logFile.createNewFile();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        try {
+            //BufferedWriter for performance, true to set append to file flag
+            BufferedWriter buf = new BufferedWriter(new FileWriter(logFile, true));
+            buf.append(stringBuilder);
+            buf.newLine();
+            buf.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
     private static String getDensityName(Context context) {
         float density = context.getResources().getDisplayMetrics().density;
         if (density >= 4.0) {
@@ -47,6 +104,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         return "ldpi";
     }
+
     public static String getDensity(Context context) {
         String r;
         DisplayMetrics metrics = new DisplayMetrics();
@@ -61,13 +119,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 r = "ldpi";
             } else if (metrics.densityDpi <= DisplayMetrics.DENSITY_MEDIUM) {
                 r = "mdpi";
-            }else if (metrics.densityDpi <= DisplayMetrics.DENSITY_HIGH) {
+            } else if (metrics.densityDpi <= DisplayMetrics.DENSITY_HIGH) {
                 r = "hdpi";
-            }else if (metrics.densityDpi <= DisplayMetrics.DENSITY_XHIGH) {
+            } else if (metrics.densityDpi <= DisplayMetrics.DENSITY_XHIGH) {
                 r = "xhdpi";
             } else if (metrics.densityDpi <= DisplayMetrics.DENSITY_XXHIGH) {
                 r = "xxhdpi";
-            }else if (metrics.densityDpi <= DisplayMetrics.DENSITY_XXXHIGH) {
+            } else if (metrics.densityDpi <= DisplayMetrics.DENSITY_XXXHIGH) {
                 r = "xxxhdpi";
             } else {
                 r = "no-dpi";
@@ -76,6 +134,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         return r;
     }
+
     @Override
     public void onClick(View v) {
         Intent intent;
